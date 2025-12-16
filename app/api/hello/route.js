1385fb8ce55e0713ff58f30ce3a28b770eb43319
DhimanMajumdar <dhimanmajumdar08233@gmail.com>
2025-12-16 14:37:34 +0530

    done query params

--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -4,37 +4,51 @@ export const users = [
   {
     id: 1,
     name: "John Doe",
-    email: "john@gmail.com",
-    age: 26,
+    email: "john@example.com",
+    age: 25,
   },
   {
-    id: 1,
-    name: "John Doe",
-    email: "john@gmail.com",
-    age: 26,
+    id: 2,
+    name: "Jane Smith",
+    email: "jane@example.com",
+    age: 30,
   },
   {
-    id: 1,
-    name: "John Doe",
-    email: "john@gmail.com",
-    age: 26,
+    id: 3,
+    name: "Bob Johnson",
+    email: "bob@example.com",
+    age: 28,
   },
 ];
 
 export async function GET(request) {
   try {
+
+    const searchParams = request.nextUrl.searchParams;
+
+    const name = searchParams.get("name") //single value
+    const age = searchParams.get("age")
+    
+    let filteredUsers = users;
+
+    if(age){
+      filteredUsers = filteredUsers.filter((user)=>user.age === Number(age))
+    }
+
+    if(name){
+       filteredUsers = filteredUsers.filter((user)=>user.name.toLowerCase().includes(name.toLowerCase()))
+    }
+
     return NextResponse.json({
-      success: true,
-      data: users,
-      total: users.length,
-    });
+        success:true,
+        data:filteredUsers,
+        total:filteredUsers.length
+    })
   } catch (error) {
-    return NextResponse.json(
-      {
-        success: false,
-        error: "Failed to get users",
-      },
-      { status: 500 }
-    );
+    return NextResponse.json({
+        success:false , error:"Failed to get users",    
+    },
+     {status:500}
+)
   }
 }
